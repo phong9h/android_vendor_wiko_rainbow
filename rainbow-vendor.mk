d8273d298d70f1dd9f88eeadc185e7636cb10134
phong 9h <tranthephong33@gmail.com>
2015-08-12 21:03:57 +0700

    First commit

---
+++ rainbow-vendor.mk
@@ -0,0 +1,12 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/gapps/system,system)
+
